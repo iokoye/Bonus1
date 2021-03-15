0577834546bb7236591d0e5d951cf3bc810baf1a
Chike Okoye <iokoye217@gmail.com>
2021-03-15 18:48:38 -0400

    ...

--- Bonus1/Bonus1.Tests/UnitTest1.cs
+++ Bonus1/Bonus1.Tests/UnitTest1.cs
@@ -14,5 +14,41 @@ namespace Bonus1.Tests
 
             Assert.Equal("Letter Grade: A", result);
         }
+        [Fact]
+        public void ConvertToGradeB()
+        {
+            var converter = new GradeConverter();
+
+            var result = converter.LetterGrade(85);
+
+            Assert.Equal("Letter Grade: B", result);
+        }
+        [Fact]
+        public void ConvertToGradeC()
+        {
+            var converter = new GradeConverter();
+
+            var result = converter.LetterGrade(78);
+
+            Assert.Equal("Letter Grade: C", result);
+        }
+        [Fact]
+        public void ConvertToGradeD()
+        {
+            var converter = new GradeConverter();
+
+            var result = converter.LetterGrade(62);
+
+            Assert.Equal("Letter Grade: D", result);
+        }
+        [Fact]
+        public void ConvertToGradeF()
+        {
+            var converter = new GradeConverter();
+
+            var result = converter.LetterGrade(50);
+
+            Assert.Equal("Letter Grade: F", result);
+        }
     }
 }
